98612b2cbf8e4536cab49db80b3a4b6a91135151
Brian Wilson <brianwilson1001@hotmail.com>
2024-06-15 14:14:49 +0100

    Show track download URL as a link

--- racedb/track/admin.py
+++ racedb/track/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Track
 
-admin.site.register(Track)
+
+class Trackadmin(admin.ModelAdmin):
+    list_display = ['name', 'download_as_link',]
+
+    def download_as_link(self, obj):
+        return format_html('<a href="{download}" target="dl_track">{download}</a>', download=obj.download)
+
+admin.site.register(Track, Trackadmin)
